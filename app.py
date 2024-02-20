8827ee86c566d262a39030b3ab86de9ccbfe9bcd
YaremenkoM <mariayaremenko13@gmail.com>
2024-02-20 15:06:17 +0100

    Create simple CLI calculator (can add and subtract)

---
+++ app.py
@@ -0,0 +1,26 @@
+def main():
+    print("This is simple calculator for two numbers.")
+    print("Available operations: ")
+    print("1 - Add")
+    print("2 - Subtract")
+
+    operation_type = input("Enter operation number: ")
+
+    try:
+        operation = int(operation_type)
+        first_number = float(input("First number: "))
+        second_number = float(input("Second number: "))
+        result = 0
+        if operation == 1:
+            result = first_number + second_number
+        elif operation == 2:
+            result = first_number - second_number
+        else:
+            print("There is no such an operation.")
+
+        print("Result: " + str(result))
+    except ValueError:
+        print("Invalid input")
+
+
+main()
